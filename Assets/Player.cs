201b74bd13e554fdf405000b346e5d39b5c2507b
lestherreynoso <lhreynos@gmail.com>
2018-08-11 13:57:52 -0400

    player moving

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed = 10f; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += hAxis * speed * Time.deltaTime;
+        pos.z += vAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+
+    }
+}
