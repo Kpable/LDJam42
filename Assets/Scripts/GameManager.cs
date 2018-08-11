8f2ca55cf5eb42a0611ae72601ef7fcf26d69677
lestherreynoso <lhreynos@gmail.com>
2018-08-11 15:59:41 -0400

    gift drop off and properties

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public ApartmentObject[] gifts;
+    int giftIndex;
+    public Transform dropOffPoint;
+    public GameObject giftPrefab;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("DropOffGift");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator DropOffGift()
+    {
+        GameObject gift = Instantiate(giftPrefab, dropOffPoint.position, Quaternion.identity);
+        var giftProperties = gift.GetComponent<Gift>();
+        giftProperties.Properties = gifts[giftIndex];
+        giftIndex++;
+        if (giftIndex < gifts.Length)
+        {
+            yield return new WaitForSeconds(3);
+            StartCoroutine("DropOffGift");
+        }
+    }
+}
+
+[System.Serializable]
+public class ApartmentObject
+{
+    public string Name;
+    //public ApartmentObjectType type;
+    public PlacableOn[] placeables;
+    public int width;
+    public int length;
+    public Vector2 Size { get { return new Vector2(width, length); } }
+}
+
+public enum PlacableOn { Floor, Furniture, Wall, Shelf }
---
+++ Assets/Scripts/Gift.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gift : MonoBehaviour {
+
+    ApartmentObject gift;
+    public ApartmentObject Properties { get { return gift; } set { gift = value; } }
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
